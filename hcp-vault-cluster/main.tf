0fce2cca4376d767095fb05bf3561ad4f8c8d469
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-16 13:59:15 +0000

    replaced value in main.tf with variables

--- hcp-vault-cluster/main.tf
+++ hcp-vault-cluster/main.tf
@@ -1,13 +1,13 @@
 resource "hcp_hvn" "virgo" {
   hvn_id         = "virgo"
   cloud_provider = "aws"
-  region         = "ap-southeast-1"
-  cidr_block     = "172.25.16.0/20"
+  region         = var.hvn_region
+  cidr_block     = var.hvn_cidr_block
 }
 
 resource "hcp_vault_cluster" "virgo_vault_cluster" {
   cluster_id      = "virgo-vault-cluster"
   hvn_id          = hcp_hvn.virgo.hvn_id
-  tier            = "starter_small"
-  public_endpoint = true
+  tier            = var.hcp_vault_cluster_tier
+  public_endpoint = var.hcp_vault_cluster_public_endpoint
 }
