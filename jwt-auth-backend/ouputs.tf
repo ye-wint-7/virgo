af6961b666edef6c13568b81a440a7e1bf7a1bcf
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-17 16:02:39 +0000

    added bound claim output

--- jwt-auth-backend/ouputs.tf
+++ jwt-auth-backend/ouputs.tf
@@ -1,3 +1,7 @@
 output "jwt_role_name" {
   value = vault_jwt_auth_backend_role.jwt_admin_role.role_name
 }
+
+output "openid_claims" {
+  value = vault_jwt_auth_backend_role.jwt_admin_role.bound_claims
+}
