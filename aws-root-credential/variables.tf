7d5d973bce3d8688cedf2ab55baf5945d41c8043
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-20 15:56:16 +0000

    change variables

--- aws-root-credential/variables.tf
+++ aws-root-credential/variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default     = "ap-southeast-1"
 }
 
-variable "aws_access_key_id" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS region"
-  type        = string
-}
-
 variable "iam_user_name" {
   description = "IAM User Name"
   type        = string
