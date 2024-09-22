06c23e38194bca4e1186c09e9a2315cc4eeda618
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-22 15:51:57 +0000

    remove data

--- aws-root-credential/main.tf
+++ aws-root-credential/main.tf
@@ -76,7 +76,7 @@ resource "vault_aws_secret_backend_role" "aws_dev_role" {
   policy_arns = ["arn:aws:iam::aws:policy/AmazonVPCFullAccess"]
 }
 
-data "vault_aws_access_credentials" "aws_dev_role_creds" {
-  backend = vault_aws_secret_backend.aws_dev.path
-  role    = vault_aws_secret_backend_role.aws_dev_role.name
-}
+# data "vault_aws_access_credentials" "aws_dev_role_creds" {
+#   backend = vault_aws_secret_backend.aws_dev.path
+#   role    = vault_aws_secret_backend_role.aws_dev_role.name
+# }
