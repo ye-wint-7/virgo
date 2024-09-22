ab9cca3f479b7d5b70ca9ddb21dfc7f17c991dae
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-22 14:34:54 +0000

    amend aws policy for vault-admin

--- aws-root-credential/main.tf
+++ aws-root-credential/main.tf
@@ -31,7 +31,8 @@ module "iam_user_root_cred_policy" {
       ],
       "Effect": "Allow",
       "Resource": [
-        "arn:aws:iam::${var.account_id}:user/${var.iam_user_name}"
+        "arn:aws:iam::${var.account_id}:user/${var.iam_user_name}",
+        "arn:aws:iam::${var.account_id}:user/vault-*"
       ]
     }
   ],
