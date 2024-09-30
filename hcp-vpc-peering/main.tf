497a0ebf2f3fbe0eeff7d29069c8e698b4893278
ye-wint-7 <ywa.falcon@gmail.com>
2024-09-30 14:36:28 +0000

    add route

--- hcp-vpc-peering/main.tf
+++ hcp-vpc-peering/main.tf
@@ -37,3 +37,9 @@ resource "aws_route" "private" {
   destination_cidr_block    = data.hcp_hvn.hvn.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
 }
+
+resource "aws_route" "db" {
+  route_table_id            = data.aws_route_table.db_rt.id
+  destination_cidr_block    = data.hcp_hvn.hvn.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
+}
